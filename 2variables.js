5f854d43ba89ffa4a7077b2686166ace03a59d10
satish <gangsatish001@gmail.com>
2026-01-27 12:41:17 +0530

    my java script practice

---
+++ 2variables.js
@@ -0,0 +1,8 @@
+const name = "Ganga Satish";
+let marks = 85;
+
+console.log(name);
+console.log(marks);
+
+marks = 90;
+console.log(marks);
---
+++ 3datatypes.js
@@ -0,0 +1,24 @@
+// Same symbols, different data types, different result.
+console.log(10 + 10);      // 20
+console.log("10" + "10"); // "1010"
+
+//Number
+// Used for:Integers, Decimals, Negative numbers
+// In JavaScript:
+//No separate int, float
+// Everything is just Number
+let age = 20;
+let price = 99.5;
+let temperature = -5;
+console.log(age);
+console.log(price);
+console.log(temperature);
+
+//String
+// Used for:Names, Sentences, Any text
+// Written using:Double quotes " ", Single quotes ' '
+let name = "Satish";
+let city = 'Kadapa';
+console.log(name);
+console.log(city);
+
