5ec58c1599519778bf42bf54ba5e5276496b553c
satish <gangsatish001@gmail.com>
2026-02-08 18:41:51 +0530

    practice storages

---
+++ 36testing_of_all_storages.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+console.log("This is a practice file.");
+ localStorage.setItem("name", "John Doe");
+ 
+ sessionStorage.setItem("sessionName", "Jane Doe");
+
+ document.cookie = "username=John Doe; expires=Fri, 31 Dec 2026  23:59:59 GMT; path=/";
+
+
